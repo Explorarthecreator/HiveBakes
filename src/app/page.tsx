c4d681ba4293e57c55bd95f5f5d007c7f74917ee
Explorarthecreator <fredadejiemma99@gmail.com>
2025-10-02 14:31:03 +0100

    feat: add fonts and placeholder text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import Image from "next/image";
+import { quickSand } from "./fonts";
+
 export default function Home() {
-  return <div>Hive bakes</div>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <Image
+        src={"/logo.svg"}
+        alt="logo"
+        width={1000}
+        height={1000}
+        className="h-20 w-20"
+      />
+      <h1
+        className={`${quickSand.className} text-3xl font-bold  animate-bounce`}
+      >
+        Work in progress
+      </h1>
+    </div>
+  );
 }
